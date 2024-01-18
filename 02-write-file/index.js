25589a66956d1e553187df1e871fa01b8f2f3164
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-01-18 19:47:01 +0100

    feat: perform the task of writing a file through the console

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const { stdout, stdin, exit } = process;
+const fs = require('fs');
+const path = require('path');
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Hello, write your text here:\n');
+stdin.on('data', (data) => {
+  if (data.toString() === 'exit\n') {
+    exit();
+  } else {
+    output.write(data);
+  }
+});
+
+process.on('exit', () => {
+  stdout.write('\nThank you, goodbye!\n');
+});
+
+process.on('SIGINT', () => {
+  exit();
+});
